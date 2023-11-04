99a512c087e968966d5359926d26bef8fe98e2fb
Jota Freitas Jr <jotafreitasjr@gmail.com>
2023-11-04 17:59:26 -0400

    End of Special Offer

--- nike/src/components/Button.jsx
+++ nike/src/components/Button.jsx
@@ -1,9 +1,17 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({
+  label,
+  iconURL,
+  backGroundColor,
+  borderColor,
+  textColor,
+}) => {
   return (
     <button
-      className="flex justify-center items-center gap-2 px-7 py-4 
+      className={`flex justify-center items-center gap-2 px-7 py-4 
     border font-montserrat text-lg leading-none 
-    bg-coral-red rounded-full text-white border-coral-red"
+    ${backGroundColor ? backGroundColor : "bg-coral-red"}
+    ${borderColor ? borderColor : "border-coral-red"}
+    ${textColor ? textColor : "text-white"} rounded-full  `}
     >
       {label}
       {iconURL && (
