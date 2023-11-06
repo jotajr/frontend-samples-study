dd222a1e571f2055075b8d5319f37aaf48e6ebaf
Jota Freitas Jr <jotafreitasjr@gmail.com>
2023-11-06 09:48:01 -0500

    Initial commit of Samba :tada:

--- nike/README.md
+++
@@ -1,8 +0,0 @@
-# Nike Portifolio APP
-
-Using the following:
-
-- Vite
-- Tailwindcss
-
-Tutorial via YouTub JS Mastery: https://www.youtube.com/watch?v=tS7upsfuxmo
--- nike/src/components/index.js
+++
@@ -1,8 +0,0 @@
-import Nav from "./Nav";
-import Button from "./Button";
-import PopularProductCard from "./PopularProductCard";
-import ServiceCard from "./ServiceCard";
-import ReviewCard from "./ReviewCard";
-import ShoeCard from "./ShoeCard";
-
-export { Nav, Button, PopularProductCard, ServiceCard, ReviewCard, ShoeCard };
